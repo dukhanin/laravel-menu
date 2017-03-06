32332582c983543a86b88e809e2571d48209bf20
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-03-06 09:08:45 +0500

    Написать тесты #LM-3 {In Progress} work 20m

---
+++ tests/MenuCollectionTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class MenuCollectionTest extends TestCase
+{
+
+    public function testEnabled()
+    {
+
+    }
+
+
+    public function testHasActive()
+    {
+
+    }
+
+
+    public function testHasEnabled()
+    {
+
+    }
+
+
+    public function testArrayAccess()
+    {
+
+    }
+
+
+    public function testBeforeWithNewItem()
+    {
+
+    }
+
+
+    public function testBeforeWithItemExists()
+    {
+
+    }
+
+
+    public function testBeforeWithNeighborNotExists()
+    {
+
+    }
+
+
+    public function testBeforeWithNullKeys()
+    {
+
+    }
+
+
+    public function testBeforeWithNestedNewItem()
+    {
+
+    }
+
+
+    public function testBeforeWithNestedItemExists()
+    {
+
+    }
+
+
+    public function testBeforeWithNestedNeighborNotExists()
+    {
+
+    }
+
+
+    public function testAfterWithNewItem()
+    {
+
+    }
+
+
+    public function testAfterWithItemExists()
+    {
+
+    }
+
+
+    public function testAfterWithNeighborNotExists()
+    {
+
+    }
+
+
+    public function testAfterWithNullKeys()
+    {
+
+    }
+
+
+    public function testAfterWithNestedNewItem()
+    {
+
+    }
+
+
+    public function testAfterWithNestedItemExists()
+    {
+
+    }
+
+
+    public function testAfterWithNestedNeighborNotExists()
+    {
+
+    }
+}
